d8820de8c21602e3cb8355826f340f8d3a916c8a
John Cross <john.william.cross@gmail.com>
2020-10-20 07:53:24 -0700

    create mock data

---
+++ src/mock-data/memory-cards.js
@@ -0,0 +1,49 @@
+const memoryCards = [
+   {
+      id: "09ef137e-10dd-466c-994d-fbcfae02cd54",
+      imagery: "A ray of sun hits the map as I plan my run.",
+      answer: "Running a map function returns an array",
+      userId: "c653e41b-c585-4d27-a439-4f9811eade44",
+      createdAt: 1603203078,
+      nextAttemptAt: 1603204278,
+      lastAttemptAt: 1603203078,
+      totalSuccessfulAttempts: 0,
+      level: 1,
+   },
+   {
+      id: "e443ae5e-66f5-4648-ae5c-3eb9fd0720b4",
+      imagery:
+         "I found a valuable surprise in decks of cards and shouted 'hooray'",
+      answer: "Use indexOf() to check if a value exists in an array",
+      userId: "5d127345-c95d-42c3-bd84-faccfe2e0004",
+      createdAt: 1603204451,
+      nextAttemptAt: 1603204458,
+      lastAttemptAt: 1603204451,
+      totalSuccessfulAttempts: 0,
+      level: 1,
+   },
+   {
+      id: "e12c1655-6023-4267-8f6e-57cfc9553a5a",
+      imagery:
+         "On the first day of school, John Smith the 10th forgot his class name so he went to the wrong class.",
+      answer: "JSX uses className instead of class",
+      userId: "c653e41b-c585-4d27-a439-4f9811eade44",
+      createdAt: 1603204529,
+      nextAttemptAt: 1603204536,
+      lastAttemptAt: 1603204529,
+      totalSuccessfulAttempts: 0,
+      level: 1,
+   },
+   {
+      id: "e12c1655-6023-4267-8f6e-57cfc9553a5a",
+      imagery:
+         "Jim Thorpe, an old footballer, is cooking hashbrowns with 8 octopus arms.",
+      answer: "The technical term for hashtag is octothorpe",
+      userId: "5d127345-c95d-42c3-bd84-faccfe2e0004",
+      createdAt: 1603204593,
+      nextAttemptAt: 1603204602,
+      lastAttemptAt: 1603204593,
+      totalSuccessfulAttempts: 0,
+      level: 1,
+   },
+];
