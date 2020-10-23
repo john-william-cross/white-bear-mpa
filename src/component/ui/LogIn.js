fe696bfd0c70b322885e4c8b81e9a7c6b265d5fd
John Cross <john.william.cross@gmail.com>
2020-10-23 16:31:05 -0700

    import hash and uuid

--- src/component/ui/LogIn.js
+++ src/component/ui/LogIn.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import hash from "object-hash";
+import { v4 as getUuid } from "uuid";
 
 export default function LogIn() {
    return (
