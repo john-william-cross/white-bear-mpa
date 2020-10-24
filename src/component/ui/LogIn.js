c419cbc0d2decc0e09ae0e0e675cf253cd19a3e6
John Cross <john.william.cross@gmail.com>
2020-10-24 11:00:33 -0700

    validate email for login (no validation for password needed)

--- src/component/ui/LogIn.js
+++ src/component/ui/LogIn.js
@@ -1,37 +1,107 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import classnames from "classnames";
+import hash from "object-hash";
+import { v4 as getUuid } from "uuid";
 
-export default function LogIn() {
-   return (
-      <div className="col-12 col-md-6">
-         <div className="card ml-md-8">
-            <div className="card-body text-dark font-sans-serif">
-               <h2 className="font-serif">Welcome Back</h2>
-               <p>Log in with your email address and password.</p>
-               <p className="lead text-muted mt-2">Email address</p>
-               <input
-                  className="form-control"
-                  id="login-email-input"
-                  type="text"
-                  placeholder=""
-               />
-               <p className="lead text-muted mt-2">Password</p>
-               <input
-                  className="form-control"
-                  id="login-password-input"
-                  type="password"
-                  placeholder=""
-               />
+export default class LogIn extends React.Component {
+   //we can set the state in constructor
+   constructor(props) {
+      super(props);
+      console.log("In a new class component!");
+      this.state = {
+         isDisplayingInputs: false,
+         emailError: "",
+         passwordError: "",
+         hasEmailError: false,
+      };
+   }
 
-               <Link
-                  to="create-answer"
-                  id="login-button"
-                  className="float-right btn btn-success btn-sm mt-2"
-               >
-                  Log in
-               </Link>
+   async setEmailState(emailInput) {
+      const lowerCasedEmailInput = emailInput.toLowerCase();
+      console.log(lowerCasedEmailInput);
+
+      // eslint-disable-next-line
+      const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (emailInput === "")
+         this.setState({
+            emailError: "Please enter your email address.",
+            hasEmailError: true,
+         });
+      else if (emailRegex.test(lowerCasedEmailInput) === false) {
+         console.log("not a valid email");
+         this.setState({
+            emailError: "Please enter a valid email address.",
+            hasEmailError: true,
+         });
+      } else {
+         this.setState({ emailError: "", hasEmailError: false });
+      }
+   }
+
+   async validateUser() {
+      // is it ok to accept blank password here?
+      // should this function be called validateAndCreateUser still?
+      const emailInput = document.getElementById("signup-email-input").value;
+
+      await this.setEmailState(emailInput);
+
+      if (this.state.hasEmailError === false) {
+         const user = {
+            id: getUuid(),
+            email: emailInput,
+            createdAt: Date.now(),
+         };
+         console.log(user);
+      }
+   }
+
+   render() {
+      return (
+         <div className="col-12 col-md-6">
+            <div className="card ml-md-8">
+               <div className="card-body text-dark font-sans-serif">
+                  <h2 className="font-serif">Welcome Back</h2>
+                  <p>Log in with your email address and password.</p>
+                  <p className="lead text-muted mt-2">Email address</p>
+                  <input
+                     id="signup-email-input"
+                     className={classnames({
+                        "form-control": true,
+                        "mb-2": true,
+                        lead: true,
+                        "is-invalid": this.state.emailError,
+                     })}
+                     type="email"
+                  />
+                  {this.state.hasEmailError && (
+                     <p className="text-danger">{this.state.emailError}</p>
+                  )}
+                  <p className="lead text-muted mt-2">Password</p>
+
+                  <input
+                     id="signup-password-input"
+                     className={classnames({
+                        "form-control": true,
+                        "text-muted": true,
+                        "mt-2": true,
+                        lead: true,
+                        "is-invalid": this.state.hasPasswordError,
+                     })}
+                     type="password"
+                  />
+                  <button
+                     to="create-answer"
+                     className="float-right btn btn-success btn-sm mt-2"
+                     id="login-button"
+                     onClick={() => {
+                        this.validateUser();
+                     }}
+                  >
+                     Log in
+                  </button>
+               </div>
             </div>
          </div>
-      </div>
-   );
+      );
+   }
 }
