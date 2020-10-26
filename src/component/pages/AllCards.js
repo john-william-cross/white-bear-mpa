637abec305f48f2304acd01f3dc3a5808f1501bc
John Cross <john.william.cross@gmail.com>
2020-10-25 17:15:40 -0700

    commit at 16 minutes in 316A

--- src/component/pages/AllCards.js
+++ src/component/pages/AllCards.js
@@ -14,12 +14,21 @@ export default class AllCards extends React.Component {
 
    filterByInput() {
       const input = document.getElementById("search-input").value;
-      const lowerCasedinput = input.toLowerCase();
-      console.log(lowerCasedinput);
+      const lowerCasedInput = input.toLowerCase();
+      console.log(lowerCasedInput);
       const copyOfMemoryCards = [...this.state.memoryCards];
-      copyOfMemoryCards.filter((memoryCard) => {
-         if (memoryCard.imagery.includes(lowerCasedinput)) {
-         }
+      const filteredMemoryCards = copyOfMemoryCards.filter((memoryCard) => {
+         const lowerCasedImagery = memoryCard.imagery.toLowerCase();
+         const lowerCasedAnswer = memoryCard.answer.toLowerCase();
+         if (
+            lowerCasedImagery.includes(lowerCasedInput) ||
+            lowerCasedAnswer.includes(lowerCasedInput)
+         ) {
+            return true;
+         } else return false;
+      });
+      this.setState({ memoryCards: filteredMemoryCards }, () => {
+         this.setMemoryCards();
       });
    }
 
@@ -41,17 +50,17 @@ export default class AllCards extends React.Component {
       this.setState({ memoryCards: orderedMemoryCards });
    }
 
-   setMemoryCardsOrder(e) {
-      console.log(`you've made a change!`); //41 minute mark is where things behave differently
-      const newOrder = e.target.value;
-      console.log(newOrder);
-      const copyOfMemoryCards = [...this.state.memoryCards];
-      const toJson = JSON.parse(newOrder);
-      console.log(...toJson);
-      const orderedMemoryCards = orderBy(copyOfMemoryCards, ...toJson);
-      console.log(orderedMemoryCards);
-      this.setState({ order: newOrder, memoryCards: orderedMemoryCards });
-   }
+   // setMemoryCardsOrder(e) {
+   //    console.log(`you've made a change!`); //41 minute mark is where things behave differently
+   //    const newOrder = e.target.value;
+   //    console.log(newOrder);
+   //    const copyOfMemoryCards = [...this.state.memoryCards];
+   //    const toJson = JSON.parse(newOrder);
+   //    console.log(...toJson);
+   //    const orderedMemoryCards = orderBy(copyOfMemoryCards, ...toJson);
+   //    console.log(orderedMemoryCards);
+   //    this.setState({ order: newOrder, memoryCards: orderedMemoryCards });
+   // }
 
    render() {
       return (
