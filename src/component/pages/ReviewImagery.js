b54bf16d605e58a8e356a74f939fc17e46537480
John Cross <john.william.cross@gmail.com>
2020-10-28 07:56:09 -0700

    commit after 318A

--- src/component/pages/ReviewImagery.js
+++ src/component/pages/ReviewImagery.js
@@ -3,32 +3,49 @@ import React from "react";
 import AppTemplate from "../ui/AppTemplate";
 import { Link } from "react-router-dom";
 import memoryCards from "../../mock-data/memory-cards";
+import axios from "axios";
 
 const memoryCard = memoryCards[3];
 
-export default function ReviewImagery() {
-   return (
-      <AppTemplate>
-         <div className="mb-5"></div>
+export default class ReviewImagery extends React.Component {
+   constructor(props) {
+      super(props);
+      axios
+         .get("https://run.mocky.io/v3/50e1b224-e38f-4e5e-9a83-8764c4b9d77a")
+         .then(function (response) {
+            // handle success
+            console.log(response);
+         })
+         .catch(function (error) {
+            // handle error
+            console.log(error);
+         });
+   }
 
-         <div className="card mb-5">
-            <div className="card-body bg-primary lead">
-               {memoryCard.imagery}
+   render() {
+      return (
+         <AppTemplate>
+            <div className="mb-5"></div>
+
+            <div className="card mb-5">
+               <div className="card-body bg-primary lead">
+                  {memoryCard.imagery}
+               </div>
             </div>
-         </div>
 
-         <Link to="review-answer" className="btn btn-link">
-            Previous card
-         </Link>
-         <div className="float-right">
-            <Link
-               to="review-answer"
-               type="button"
-               className="btn btn-outline-primary"
-            >
-               Show Answer
+            <Link to="review-answer" className="btn btn-link">
+               Previous card
             </Link>
-         </div>
-      </AppTemplate>
-   );
+            <div className="float-right">
+               <Link
+                  to="review-answer"
+                  type="button"
+                  className="btn btn-outline-primary"
+               >
+                  Show Answer
+               </Link>
+            </div>
+         </AppTemplate>
+      );
+   }
 }
