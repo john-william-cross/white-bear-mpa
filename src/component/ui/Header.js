1f218714680da30528f8a3d04007548e0f91c7cc
John Cross <john.william.cross@gmail.com>
2020-11-01 15:19:56 -0800

    end of 327A Updating the Current User in Redux

--- src/component/ui/Header.js
+++ src/component/ui/Header.js
@@ -1,18 +1,40 @@
 import React from "react";
 import appLogo from "../../icons/logo-app.svg";
 import { Link } from "react-router-dom";
+import actions from "../../store/actions";
+import { connect } from "react-redux";
 
-export default function Header() {
-   return (
-      <>
-         <img src={appLogo} width="32px" alt="White Bear logo" />
-         <h3 className="d-inline text-brand text-dark ml-1">White Bear</h3>
-         <Link to="/" className="btn btn-link float-right">
-            Log out
-         </Link>
-         <div className="clearfix"></div>
-      </>
-   );
+class Header extends React.Component {
+   logOutOutCurrentUser() {
+      this.props.dispatch({
+         type: actions.UPDATE_CURRENT_USER,
+         payload: {}, //payload is empty object
+      });
+   }
+
+   render() {
+      return (
+         <>
+            <img src={appLogo} width="32px" alt="White Bear logo" />
+            <h3 className="d-inline text-brand text-dark ml-1">White Bear</h3>
+            <Link
+               to="/"
+               className="btn btn-link float-right"
+               onClick={() => {
+                  this.logOutOutCurrentUser(); //running this function updates the redux store, which is our global state
+               }}
+            >
+               Log out
+            </Link>
+            <div className="clearfix"></div>
+         </>
+      );
+   }
 }
 
-//comment
+function mapStateToProps(state) {
+   //Everything down here is global state
+   return {};
+}
+
+export default connect(mapStateToProps)(Header);
