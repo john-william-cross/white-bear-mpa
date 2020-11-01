3a72a21c4ed51864f0218b28d7c4528b8417266e
John Cross <john.william.cross@gmail.com>
2020-10-31 18:54:28 -0700

    commit at 1hour 18 minutes 325A

--- src/component/pages/ReviewImagery.js
+++ src/component/pages/ReviewImagery.js
@@ -29,6 +29,9 @@ class ReviewImagery extends React.Component {
                console.log(error);
             });
       }
+      if (props.queue.index > props.queue.cards.length) {
+         this.props.history.push("/review-empty ");
+      }
    }
 
    goToPrevCard() {
